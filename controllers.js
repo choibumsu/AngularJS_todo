750245372479b0c260a82ea59c66e0f8798a7dc8
choibumsu <bumsu0211@gmail.com>
2020-01-03 14:33:41 +0900

    script.js 모듈화

---
+++ controllers.js
@@ -0,0 +1,41 @@
+angular.module('todo').controller('TodoCtrl', ['$scope', function($scope) {
+    $scope.todos = [
+      {
+        title: "앵귤러 배우기",
+        completed : true,
+        createdAt: Date.now()
+      },
+      {
+        title: "스프링 배우기",
+        completed : false,
+        createdAt: Date.now()
+      },
+      {
+        title: "점심 먹기",
+        completed : false,
+        createdAt: Date.now()
+      },
+    ];
+    
+    $scope.remove = function(todo) {
+      var idx = $scope.todos.findIndex(function (item) {
+        return item.title === todo.title;
+      });
+      
+      if (idx > -1) {
+        $scope.todos.splice(idx, 1);
+      }
+    }
+    
+    $scope.add = function (newTodoTitle) {
+      var newTodo = {
+        title : newTodoTitle,
+        completed : false,
+        createdAt : Date.now()
+      };
+      
+      $scope.todos.push(newTodo);
+      
+      $scope.newTodoTitle = "";
+    };
+  }]);
